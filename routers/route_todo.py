1a08e0722e5a6791e15b7450dc9c29a084c91c38
Hayato <hayato.314.nico@gmail.com>
2023-11-04 07:24:10 +0900

    UpdateとDeleteの作成

--- routers/route_todo.py
+++ routers/route_todo.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter
 from fastapi import Request,Response,HTTPException
 from fastapi.encoders import jsonable_encoder
-from schemas import Todo,TodoBody
+from schemas import Todo,TodoBody,SuccessMsg
 from starlette.status import HTTP_201_CREATED
-from database import db_create_todo,db_get_todos,db_get_single_todo
+from database import db_create_todo,db_get_todos,db_update_todo,db_get_single_todo,db_delete_todo
 from typing import List
 router = APIRouter()
 
@@ -32,3 +32,19 @@ async def get_single_todo(id:str):
     raise HTTPException(
         #f=文字列と変数の結合を示す
         status_code=404, detail=f"Task of ID:{id} dosen't exist")
+
+@router.put("/api/todo/{id}",response_model=Todo)
+async def update_todo(id:str,data:TodoBody):
+    todo = jsonable_encoder(data)
+    res = await db_update_todo(id,todo)
+    if res:
+        return res
+    raise HTTPException(
+        status_code=404, detail=f"Task of ID:{id} dosen't exist")
+
+@router.delete("/api/todo/{id}",response_model=SuccessMsg)
+async def delete_todo(id:str):
+    res = await db_delete_todo(id)
+    if res:
+        return {'message': 'Successfully deleted'}
+    raise HTTPException(status_code=404, detail=f"Delete task failed")
